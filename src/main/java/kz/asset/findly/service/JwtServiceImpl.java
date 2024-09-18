52c5f8c74b68f2f0fe78eea66f445168f3d3e758
Asset <a.sakitzhanov@gmail.com>
2024-09-18 17:04:46 +0500

    Added roles to token

--- src/main/java/kz/asset/findly/service/JwtServiceImpl.java
+++ src/main/java/kz/asset/findly/service/JwtServiceImpl.java
@@ -28,7 +28,14 @@ public class JwtServiceImpl implements JwtService {
 
 	@Override
 	public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
+		Map<String, Object> extraClaims = new HashMap<>();
+		
+		extraClaims.put("roles", userDetails.getAuthorities().stream()
+					.map(item -> item.getAuthority())
+					.toList()
+				);
+		
+        return generateToken(extraClaims, userDetails);
 	}
 
 	@Override
